6051eeaa3795ae54dc6eb0b4f1f07f2f82e3b36b
phamtung86 <139665713+phamtung86@users.noreply.github.com>
2024-11-17 12:23:05 +0700

    [add] them giao dien quan ly voucher

--- Backend/src/main/java/org/example/modal/Voucher.java
+++ Backend/src/main/java/org/example/modal/Voucher.java
@@ -2,6 +2,8 @@ package org.example.modal;
 
 import jakarta.persistence.*;
 
+import java.sql.Timestamp;
+
 @Entity
 @Table(name = "voucher")
 public class Voucher {
@@ -20,6 +22,14 @@ public class Voucher {
     @Column(name = "type")
     private int type; // 0: phan tram - 1: Gia tri tien
 
+    @Column(name = "status")
+    private int status; // 0: Het han  1:Active -1: Khoa
+
+    @Column(name = "start_date")
+    private Timestamp startDate;
+
+    @Column(name = "expiry_date")
+    private Timestamp expiryDate;
     public Voucher() {}
 
     public int getId() {
@@ -54,6 +64,30 @@ public class Voucher {
         this.type = type;
     }
 
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Timestamp startDate) {
+        this.startDate = startDate;
+    }
+
+    public Timestamp getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Timestamp expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
     @Override
     public String toString() {
         return "Voucher{" +
@@ -61,6 +95,9 @@ public class Voucher {
                 ", voucherName='" + voucherName + '\'' +
                 ", value=" + value +
                 ", type=" + type +
+                ", status=" + status +
+                ", startDate=" + startDate +
+                ", expiryDate=" + expiryDate +
                 '}';
     }
 }
